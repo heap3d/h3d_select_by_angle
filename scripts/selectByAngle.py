3e185210afb9fc5255d7c57bf13098a017c3e6cb
Dmytro Holub <heap3d@gmail.com>
2024-09-07 01:40:44 +0300

    refactoring: reorder PolygonSelector methods

--- scripts/selectByAngle.py
+++ scripts/selectByAngle.py
@@ -37,13 +37,6 @@ def expand_fill():
     poly_selector.selection_expand_fill()
 
 
-def set_angle_userval():
-    try:
-        lx.eval("user.value %s" % USERVAL_NAME_ANGLE)
-    except RuntimeError:
-        pass
-
-
 def expand_once():
     angle = get_user_value(USERVAL_NAME_ANGLE)
     meshes = get_selected_meshes()
@@ -52,6 +45,13 @@ def expand_once():
         raise ValueError('angle is None')
     poly_selector = PolygonSelector(polygons=polygons, angle=angle)
     poly_selector.selection_expand_once()
+
+
+def set_angle_userval():
+    try:
+        lx.eval("user.value %s" % USERVAL_NAME_ANGLE)
+    except RuntimeError:
+        pass
 
 
 def get_selected_meshes() -> list[modo.Mesh]:
@@ -89,56 +89,6 @@ class PolygonSelector:
         self.edge_of_selection = dict()
         # set failed pairs list
         self.failed_pairs = []
-
-    def add_to_failed(self, poly1, poly2):
-        """add polygons in ascneding order"""
-        if not all((poly1, poly2)):
-            return
-        if poly1.index < poly2.index:
-            self.failed_pairs.append((poly1, poly2))
-        else:
-            self.failed_pairs.append((poly2, poly1))
-
-    def in_failed_pairs(self, poly1, poly2):
-        """check in ascending order"""
-        if poly1.index < poly2.index:
-            return (poly1, poly2) in self.failed_pairs
-        else:
-            return (poly2, poly1) in self.failed_pairs
-
-    def add_to_selection(self, in_polygon):
-        """add polygon to selection dict"""
-        self.selection[in_polygon.id] = in_polygon
-
-    def add_to_preselection(self, in_polygon):
-        self.preselection[in_polygon.id] = in_polygon
-
-    def is_selected(self, in_polygon):
-        """check if polygon selected by mark"""
-        result = in_polygon.id in self.selection
-        return result
-
-    def is_pre_selected(self, in_polygon):
-        result = in_polygon.id in self.preselection
-        return result
-
-    def is_on_selection_edge(self, in_polygon):
-        result = in_polygon.id in self.edge_of_selection
-        return result
-
-    def can_select_by_angle(self, in_polygon, new_poly):
-        """compare two polygon normals"""
-        in_vector = modo.Vector3(in_polygon.normal)
-        compare_vector = modo.Vector3(new_poly.normal)
-        if compare_vector == in_vector:
-            return True
-        try:
-            compare_angle = compare_vector.angle(in_vector)
-        except ValueError:
-            return True
-
-        r_value = compare_angle <= self.threshold
-        return r_value
 
     def selection_expand_fill(self):
         # expand selection by one step first
@@ -193,15 +143,56 @@ class PolygonSelector:
         for polygon in self.preselection.values():
             polygon.select()
 
+    def add_to_selection(self, in_polygon):
+        """add polygon to selection dict"""
+        self.selection[in_polygon.id] = in_polygon
+
+    def is_pre_selected(self, in_polygon):
+        result = in_polygon.id in self.preselection
+        return result
+
+    def is_on_selection_edge(self, in_polygon):
+        result = in_polygon.id in self.edge_of_selection
+        return result
+
+    def is_selected(self, in_polygon):
+        """check if polygon selected by mark"""
+        result = in_polygon.id in self.selection
+        return result
+
+    def can_select_by_angle(self, in_polygon, new_poly):
+        """compare two polygon normals"""
+        in_vector = modo.Vector3(in_polygon.normal)
+        compare_vector = modo.Vector3(new_poly.normal)
+        if compare_vector == in_vector:
+            return True
+        try:
+            compare_angle = compare_vector.angle(in_vector)
+        except ValueError:
+            return True
+
+        r_value = compare_angle <= self.threshold
+        return r_value
+
+    def in_failed_pairs(self, poly1, poly2):
+        """check in ascending order"""
+        if poly1.index < poly2.index:
+            return (poly1, poly2) in self.failed_pairs
+        else:
+            return (poly2, poly1) in self.failed_pairs
+
+    def add_to_preselection(self, in_polygon):
+        self.preselection[in_polygon.id] = in_polygon
+
 
 def main():
     print("")
     print("start...")
 
     action = {
-        ARG_SET: set_angle_userval,
         ARG_FILL: expand_fill,
         ARG_ONCE: expand_once,
+        ARG_SET: set_angle_userval,
     }
 
     action.get(lx.args(), expand_fill)()
